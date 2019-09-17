c5539ae60f7080460f1645175bf7c118f5657011
D. Rogers <danny.rog@live.com>
2019-09-17 17:58:56 -0500

    deleted fill project folder

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//Set temp == r0
+@R0
+D=M
+@temp
+M=D
+
+//Set sum == 0
+@R2
+M=0
+
+(MULTIPLY)
+	//if (temp == 0 goto END)
+	@temp
+	D=M
+	@END
+	D;JEQ
+
+	//add r1 to sum
+	@R1
+	D=M
+	@R2
+	M=D+M
+	@temp
+	M=M-1
+
+	//Go multiply
+	@MULTIPLY
+	0;JMP
+
+(END)
+	@END
+	0;JMP
